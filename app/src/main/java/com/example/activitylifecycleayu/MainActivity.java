a8d0ae09e962be262dd891975f390ecbbb1dd300
istianaAyu <istianaayu28@gmail.com>
2019-02-14 10:42:51 +0700

    add override

--- app/src/main/java/com/example/activitylifecycleayu/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycleayu/MainActivity.java
@@ -12,5 +12,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Overridet
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
